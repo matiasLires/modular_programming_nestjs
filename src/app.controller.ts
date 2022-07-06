faf582eb7169fa8ca50539c5dc7817dd8dc2418a
matiasLires01 <lires.matiash@gmail.com>
2022-07-05 22:35:34 -0300

    Separación de responsabilidades

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,31 +14,4 @@ export class AppController {
 	newEndPoint() {
 		return 'Hello Endpoint';
 	}
-
-	@Get('products')
-	getProducts(
-		@Query('limit') limit = 100,
-		@Query('offset') offset: number,
-		@Query('brand') brand: string,
-	) {
-		return `paginated list products: limit =>${limit} & offset =>${offset} & brand =>${brand}`;
-	}
-
-	@Get('products/filter')
-	getProductFilter() {
-		return `filter`;
-	}
-
-	@Get('products/:productId')
-	getProduct(@Param('productId') productId: string) {
-		return `product ${productId}`;
-	}
-
-	@Get('categories/:categoryId/products/:productId')
-	getCategory(
-		@Param('categoryId') categoryId: string,
-		@Param('productId') productId: string,
-	) {
-		return `category ${categoryId} and product ${productId}`;
-	}
 }
