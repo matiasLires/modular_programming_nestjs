b0024ec2750d0bf92809d1941711b5e6256e9786
matiasLires01 <lires.matiash@gmail.com>
2022-07-10 17:18:49 -0300

    Validación de esquemas en .envs con Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import { lastValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
 import { enviroments } from './enviroments';
 import config from './config';
 @Module({
@@ -15,6 +17,11 @@ import config from './config';
 			envFilePath: enviroments[process.env.NODE_ENV] || '.env',
 			isGlobal: true,
 			load: [config],
+			validationSchema: Joi.object({
+				API_KEY: Joi.number().required(),
+				DATABASE_NAME: Joi.string().required(),
+				DATABASE_PORT: Joi.number().required(),
+			}),
 		}),
 		HttpModule,
 		UsersModule,
